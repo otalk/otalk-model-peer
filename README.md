5fbf30b6abda20fc2c3a800cec7b24f39748d913
Lance Stout <lancestout@gmail.com>
2014-09-10 12:17:07 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# otalk-model-peer
+
+An Ampersand state model representing an XMPP entity.
+
+## Installing
+
+```sh
+$ npm install otalk-model-peer
+```
+
+## Properties
+
+- `caps` - `{State}`
+- `chatState` - `{String}`
+- `discoItems` - `{Collection}`
+- `errorCondition` - `{String}`
+- `errorMessage` - `{String}`
+- `id` - `{String}`
+- `idleSince` - `{Date}`
+- `isSelf` - `{Boolean}`
+- `jid` - `{JID}`
+- `lastSentMessageID` - `{String}`
+- `messages` - `{Collection}`
+- `offline` - `{Boolean}`
+- `online` - `{Boolean}`
+- `priority` - `{Number}`
+- `requestingAttention` - `{Boolean}`
+- `show` - `{String}`
+- `status` - `{String}`
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,72 @@
+var JIDMixin = require('ampersand-jid-datatype-mixin');
+var State = require('ampersand-state');
+var Collection = require('ampersand-collection');
+var Message = require('otalk-model-message');
+var DiscoInfo = require('otalk-model-disco');
+
+
+module.exports = State.extend({dataTypes: JIDMixin.dataTypes}, {
+    props: {
+        jid: 'jid',
+        isSelf: 'boolean',
+        show: {
+            type: 'string',
+            values: [
+                'dnd',
+                'xa',
+                'away',
+                'chat',
+                ''
+            ]
+        },
+        status: 'string',
+        priority: ['number', true, 0],
+        avatarID: 'string',
+        online: 'boolean',
+        errorCondition: 'string',
+        errorMessage: 'string',
+        idleSince: 'date',
+        requestingAttention: 'boolean',
+        chatState: {
+            type: 'string',
+            values: [
+                'composing',
+                'paused',
+                'active',
+                'inactive',
+                'gone',
+                ''
+            ]
+        },
+        lastSentMessageID: 'string'
+    },
+
+    derived: {
+        id: {
+            deps: ['jid'],
+            fn: function () {
+                return this.jid.full;
+            }
+        },
+        offline: {
+            deps: ['online'],
+            fn: function () {
+                return !this.online;
+            }
+        }
+    },
+
+    children: {
+        caps: DiscoInfo
+    },
+
+    collections: {
+        discoItems: Collection.extend({
+            model: DiscoInfo
+        }),
+        messages: Collection.extend({
+            model: Message,
+            comparator: 'comparator'
+        })
+    }
+});
